92fd5928955302290cdc3b3eb7cd21d6db01857e
Alejandro J Roche Benitez <rochebenitez90@gmail.com>
2024-03-26 22:26:54 +0100

    Dashboard total count

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,12 @@
 import UpcomingEvents from "./home/upcoming-events";
 import DealsChart from "./home/deals-chart";
+import DashboardTotalCountCard from "./home/total-count-card";
 import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
 import AccordionHeaderSkeleton from "./skeleton/accordion-header";
 import KanbanColumnSkeleton from "./skeleton/kanban";
 import ProjectCardSkeleton from "./skeleton/project-card";
 import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+
 
 export {
     UpcomingEvents,
@@ -14,4 +16,5 @@ export {
     KanbanColumnSkeleton,
     ProjectCardSkeleton,
     LatestActivitiesSkeleton,
+    DashboardTotalCountCard
 }
