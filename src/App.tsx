b7c1a640d38a22fdb21aaa22f694ce6aec2bc9c6
Alejandro J Roche Benitez <rochebenitez90@gmail.com>
2024-04-10 21:46:42 +0200

    edit companies component

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,7 @@ import { Home, ForgotPassword, Login, Register, CompanyList } from './pages'
 import Layout from "./components/layout";
 import { resources } from "./config/resources";
 import Create from "./pages/company/create";
+import EditPage from "./pages/company/edit";
 
 function App() {
   return (
@@ -72,6 +73,7 @@ function App() {
                   <Route path="/companies">
                     <Route index element={<CompanyList />} />
                     <Route path="new" element={<Create />} />
+                    <Route path="edit/:id" element={<EditPage />} />
                   </Route>
 
                 </Route>
