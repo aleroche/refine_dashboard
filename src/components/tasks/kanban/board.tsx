d1a6b9e29911ae7eb9988e6ec2b598aae65f22c9
Alejandro J Roche Benitez <rochebenitez90@gmail.com>
2024-04-16 09:27:17 +0200

    finish kanban drag and drop view

--- src/components/tasks/kanban/board.tsx
+++ src/components/tasks/kanban/board.tsx
@@ -1,5 +1,6 @@
-import { DndContext } from '@dnd-kit/core'
+import { DndContext, DragEndEvent, MouseSensor, TouchSensor, useSensor, useSensors } from '@dnd-kit/core'
 import React from 'react'
+
 
 export const KanbanBoardContainer = ({ children }: React.PropsWithChildren) => {
     return (
@@ -22,9 +23,28 @@ export const KanbanBoardContainer = ({ children }: React.PropsWithChildren) => {
     )
 }
 
-export const KanbanBoard = ({ children }: React.PropsWithChildren) => {
+type Props = {
+    onDragEnd: (event: DragEndEvent) => void
+}
+export const KanbanBoard = ({ children, onDragEnd }: React.PropsWithChildren<Props>) => {
+
+    const mouseSensor = useSensor(MouseSensor, {
+        activationConstraint: {
+            distance: 5
+        },
+
+    })
+
+    const touchSensor = useSensor(TouchSensor, {
+        activationConstraint: {
+            distance: 5
+        }
+    })
+
+    const sensors = useSensors(touchSensor, mouseSensor)
+
     return (
-        <DndContext>
+        <DndContext onDragEnd={onDragEnd} sensors={sensors}>
             {children}
         </DndContext>
     )
